33f6b67c8b3546f9e4ae434ef0236745d6d5311e
Thawat09 <97440224+Thawat09@users.noreply.github.com>
2022-06-07 02:46:05 +0700

    updateDate65/06/06
    Update Date 65/06/06

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+const productsRouter = require('./src/router/productsRouter');
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.set('views', "./src/views");
+app.set("view engine", "ejs");  
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index', {username: 'Thawat', customers: ["A", "B", "C"]});
 })
 
-app.listen(port, () => {
-    debug('listening on port %d' + chalk.green(" : " + port));
+app.use('/products', productsRouter);
+
+app.listen(PORT, () => {
+    debug('listening on port %d' + chalk.green(" : " + PORT));
 })
